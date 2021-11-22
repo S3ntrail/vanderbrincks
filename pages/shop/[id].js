33eef90e841d975594be481bbeb64472b0bc1029
S3ntrail <anthony.man10@hotmail.com>
2021-11-22 15:46:34 +0100

    Fetching a single item
    You are now able to fetch a single item. Just need to do some styling and then its done

--- pages/shop/[id].js
+++ pages/shop/[id].js
@@ -1,6 +1,32 @@
 import Header from '../../components/Global/Head/head'
+import data from 'data/data.json'
 
-const Details = () => {
+export const getStaticPaths = async () => {
+  const res = data
+
+  const paths = res.map(product => {
+    return {
+      params : { id: product.id.toString() }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id
+  const res = data.find(x => x.id == id)
+
+  return {
+    props : { product: res}
+  }
+
+}
+
+const Details = ({ product }) => {
 
   return (
 
@@ -10,6 +36,11 @@ const Details = () => {
       <main className="flex flex-col justify-center w-full flex-1 text-center">
 
         <section>
+          <h1>{product.id}</h1>
+          <h1>{product.name}</h1>
+          <h1>{product.price}</h1>
+          <h1>{product.durability}</h1>
+          <h1>{product.brand}</h1>
         </section>
 
       </main>
